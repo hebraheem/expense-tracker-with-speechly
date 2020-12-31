2e0f42b6845f48e907a2cd5896db7d0464cb8802
hebraheem <iadebiyi01@gmail.com>
2020-12-31 18:29:25 +0100

    added chat display

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Card, CardContent, CardHeader, Typography } from "@material-ui/core";
 import { Doughnut } from "react-chartjs-2";
+import useTransactions from "../../useTransactions";
 
 import {useStyles} from './styles';
 
 const Details = ({title}) => {
+    const {total, chatData} = useTransactions(title)
     const classes = useStyles();
   return (
     <Card className={title=== 'Income' ? classes.income: classes.expense}>
       <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">$50</Typography>
-        {/* <Doughnut data="Data"/> */}
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chatData}/>
       </CardContent>
     </Card>
   );
--- src/useTransactions.js
+++ src/useTransactions.js
@@ -5,6 +5,34 @@ const useTransactions = (title)=>{
     resetCategories();
     const { transactions } = useGlobalContext();
     // to get the exact category to add to
-    const selectedCategories = transactions.filter(trans => trans.type === title); // title will be pass from function
+    const allCategories = transactions.filter(trans => trans.type === title); // title will be pass from function
+    const total = allCategories.reduce((acc, curVal) => acc += curVal.amount, 0);
 
+    const categories =
+      title === "Income" ? incomeCategories : expenseCategories;
+
+    console.log({ allCategories, total, categories });
+
+    //loop through the allCategories to get each category where title = catrgory
+    allCategories.forEach(trans =>{
+      const category = categories.find((cat) => cat.type === trans.category);
+      // to increase the amount
+      if (category) {
+          category.amount += trans.amount
+      }
+    })
+    //to remove category of amount lessthan or equal to 0
+    const filteredCat = categories.filter((cat)=> cat.amount > 0);
+
+    // to put all data inside the chatdata;
+    const chatData= {
+        datasets: [{
+            data: filteredCat.map(c => c.amount),
+            backgroundColor: filteredCat.map(c => c.color) 
+        }],
+        labels: filteredCat.map(c => c.type)
+    }
+    return {filteredCat, total, chatData}
 }
+
+export default useTransactions;
